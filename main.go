88fd042c3a4e41d19102183911ff021ac7ff46de
Justin Zandbergen <justin.zandbergen@topicus.nl>
2025-10-03 14:27:31 +0200

    chore: remove test secret for security scanning

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import "fmt"
 var version string = "dev"
 
 func main() {
-	// This is a fake AWS access key for testing Trivy secret detection
-	// AKIAIOSFODNN7EXAMPLE
 	fmt.Println("This is go-release-please version: " + version)
 	fmt.Println("Look at this awesome feature...")
 }
